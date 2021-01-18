dcd22169f1e47a96521d978548a3b233494201de
Andrii Adamchuk <andrii.adamchuk@gmail.com>
2021-01-18 22:05:27 +0100

    data scraping update

--- selenium_data_scraper.py
+++ selenium_data_scraper.py
@@ -12,13 +12,14 @@ from selenium import webdriver
 import time
 import pandas as pd
 
-def get_apartments(city, num_apartments, verbose, slp_time):
+def get_apartments(num_apartments, verbose, slp_time):
     
 ##Running webdriver
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(executable_path='/Users/andrii/Documents/Git/ds_apartment_price_estimationa_project/chromedriver', options=options)
-    driver.set_window_size(1200, 1200)
-    url = 'https://www.spotahome.com/'+ city +''
+    chrome_options = webdriver.ChromeOptions() 
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']) 
+    driver = webdriver.Chrome(executable_path='/Users/andrii/Documents/Git/ds_apartment_price_estimationa_project/chromedriver', options=chrome_options)
+    driver.set_window_size(1600, 1000)
+    url = 'https://www.spotahome.com/berlin/page:10'
     driver.get(url)
     apartments = []
     
@@ -28,12 +29,13 @@ def get_apartments(city, num_apartments, verbose, slp_time):
         #Let the page load. Change this number based on your internet speed.
         #Or, wait until the webpage is loaded, instead of hardcoding it.
         
-        try:
-            driver.find_element_by_id("onetrust-accept-btn-handler").click()
-        except ElementClickInterceptedException:
-            pass
+        #try:
+            #driver.find_element_by_id("onetrust-accept-btn-handler").click()
+        #except ElementClickInterceptedException:
+            #pass
         
-        time.sleep(slp_time)
+        #time.sleep(slp_time)
+        
         
         #Going through each job in this page
         apartment_list = driver.find_elements_by_class_name('l-list__item')
@@ -53,11 +55,15 @@ def get_apartments(city, num_apartments, verbose, slp_time):
                     #main_title = driver.find_element_by_xpath('.//div[@class="property-title"]').text
                     main_title = driver.find_element_by_xpath('.//h1[@class="root__Heading___OmV4c dark__Heading___OmV4c large__Heading___OmV4c left__Heading___OmV4c inline__Heading___OmV4c property-title__heading"]').text
                     property_type = driver.find_element_by_xpath('.//div[@class="property-title__details"]').text
-                    monthly_rent = driver.find_element_by_xpath('.//div[@class="list-row list-row--withBorder"]').text                
+                    monthly_rent = driver.find_element_by_xpath('.//div[@class="list-row list-row--withBorder"]').text  
+                    time.sleep(slp_time)
                     bills = driver.find_element_by_xpath('.//div[@class="bill-description__title__wrapper"]').text
                     property_features = driver.find_element_by_xpath('.//div[@class="listing-tag-container"]').text
                     deposit = driver.find_element_by_xpath('.//b[@class="listing-pricing-structured__security-deposit"]').text
-                    property_rules = driver.find_element_by_xpath('.//div[@class="property-rules__collapsible__description"]').text
+                    try:
+                        salary_estimate = property_rules = driver.find_element_by_xpath('.//div[@class="property-rules__collapsible__description"]').text
+                    except NoSuchElementException:
+                        salary_estimate = -1 #You need to set a "not found value. It's important."
                     #print('collected main_info')
                     collected_successfully = True
                 except:
@@ -70,12 +76,13 @@ def get_apartments(city, num_apartments, verbose, slp_time):
                 print("bills: {}".format(bills[:500]))
                 print("property_features: {}".format(property_features[:500]))
                 print("deposit: {}".format(deposit[:500]))
-                print("property_rules: {}".format(property_rules[:500]))
+                print("property_rules: {}".format(property_rules[:1000]))
                 print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
                 #print("Title_2: {}".format(title_2[:500]))
                 #print("Price: {}".format(price[:500]))
    
-
+            time.sleep(slp_time)
+            
             try:
                 driver.find_element_by_xpath('.//button[@class="button__Button___OmV4c alt__Button___OmV4c secondary__Button___OmV4c normal__Button___OmV4c listing-preview-header__button"]').click()  #clicking to the X.
                 #print('X button clicked')
@@ -94,12 +101,12 @@ def get_apartments(city, num_apartments, verbose, slp_time):
             
             #Clicking on the "next page" button
         try:
-            driver.find_element_by_xpath('.//l-list__item[@class="next"]//a').click()
+            driver.find_element_by_xpath('.//div[@class="paginator"]').click()
         except NoSuchElementException:
             print("Scraping terminated before reaching target number of jobs. Needed {}, got {}.".format(num_apartments, len(apartments)))
             break
             
     return pd.DataFrame(apartments)
 
-df = get_apartments('berlin', 2, False, 3)
-df
+df = get_apartments(500, False, 9)
+df.to_csv("df_apartments_1.csv") 
