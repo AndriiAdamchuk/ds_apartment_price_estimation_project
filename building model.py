2bb8f7d625c22f6213473fb0d9f870998b248132
Andrii Adamchuk <andrii.adamchuk@gmail.com>
2021-01-26 22:23:12 +0100

    Update building model.py

--- building model.py
+++ building model.py
@@ -6,6 +6,8 @@ Created on Tue Jan 26 20:04:33 2021
 @author: andrii
 """
 #https://www.statsmodels.org/stable/generated/statsmodels.regression.linear_model.OLS.html
+#https://dss.princeton.edu/online_help/analysis/interpreting_regression.htm
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,7 +19,7 @@ df = pd.read_csv('df_final.csv')
 from sklearn.model_selection import train_test_split
 X = df.drop('monthly_price_avg', axis=1)
 y = df['monthly_price_avg'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 
 #Multiple Linear Regression
@@ -27,7 +29,7 @@ X_sm = X = sm.add_constant(X)
 model = sm.OLS(y,X_sm)
 model.fit().summary()
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import cross_val_score
 
 lm = LinearRegression()
@@ -36,15 +38,45 @@ lm.fit(X_train,y_train)
 np.mean(cross_val_score(lm,X_train, y_train, scoring = 'neg_mean_absolute_error', cv = 3))
 
 
-
-
-
-
-
-
 #Lasso Regression
-#ElasticNet
+lm_l = Lasso()
+np.mean(cross_val_score(lm_l,X_train, y_train, scoring = 'neg_mean_absolute_error', cv = 3))
+
+
 #Random Forest
+from sklearn.ensemble import RandomForestRegressor
+
+rf = RandomForestRegressor()
+np.mean(cross_val_score(rf,X_train, y_train, scoring = 'neg_mean_absolute_error', cv = 3))
+
+
 #RidgeRegression
+from sklearn.linear_model import Ridge
+r = Ridge()
+np.mean(cross_val_score(r,X_train, y_train, scoring = 'neg_mean_absolute_error', cv = 3))
+
+
+#ElasticNet
+from sklearn.linear_model import ElasticNet
+
+en = ElasticNet()
+np.mean(cross_val_score(en,X_train, y_train, scoring = 'neg_mean_absolute_error', cv = 3))
+
+
 #Support Vector Regression (kernel='linear')
+from sklearn import svm
+
+svm = svm.SVR()
+np.mean(cross_val_score(svm,X_train, y_train, scoring = 'neg_mean_absolute_error', cv = 3))
+
 #tune models GridsearchCV
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10,300,10), 'criterion':('mse', 'mae'), 'max_features':('auto', 'sqrt', 'log2')}
+
+gs = GridSearchCV(rf,parameters,scoring='neg_mean_absolute_error', cv=3)
+gs.fit(X_train,y_train)
+
+gs.best_score_
+gs.best_estimator_
+
+
